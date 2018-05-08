61657e5ef3a074a6a7204176b6d8f1efdd3f1655
Christophe Kamphaus <christophe.kamphaus@jemmic.com>
2018-05-08 15:47:16 +0200

    Added missing functions

---
+++ pcre_added.go
@@ -0,0 +1,28 @@
+package libpcre
+
+import "fmt"
+
+func NotBOOL(x BOOL) BOOL {
+	if x == 0 {
+		return 1
+	}
+
+	return 0
+}
+
+func InterfaceToByteSliceSlice(x interface{}) [][]byte {
+	return x.([][]byte)
+}
+
+func InterfaceToPcre_uint8SliceSlice(x interface{}) [][]pcre_uint8 {
+	return x.([][]pcre_uint8)
+}
+
+func InterfaceToPcre_ucharSliceSlice(x interface {}) [][]pcre_uchar {
+	return x.([][]pcre_uchar)
+}
+
+func main () {
+	fmt.Println("Build successfull")
+}
+
